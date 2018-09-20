5d69822e183cde505f05b4424ad68ae2e6cb164b
Diego Jancic <jancic@gmail.com>
2018-09-20 19:09:20 -0300

    updated stats

--- functions/chalice/website/app.py
+++ functions/chalice/website/app.py
@@ -10,23 +10,3 @@ def index():
 					status_code=200,
 					headers={'Content-Type': 'text/html'})
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
