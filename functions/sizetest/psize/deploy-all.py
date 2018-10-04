e5903eb08aef7da3b74df8b2b1931bdb62dbe7d9
Diego Jancic <jancic@gmail.com>
2018-10-04 20:05:49 -0300

    added lambda to test package sizes

---
+++ functions/sizetest/psize/deploy-all.py
@@ -0,0 +1,26 @@
+import os
+
+fileNames = ["app.py", ".chalice/config.json", "create-file.sh"]
+
+for sizeMb in range(0, 50):
+
+    print(f"PUBLISHING APP FOR SIZE {sizeMb}MB")
+    print("----------------------------------")
+    for fileName in fileNames:
+
+        # Read in the file
+        with open(fileName + '.template', 'r') as file:
+          filedata = file.read()
+
+        # Replace all the variables
+        sizeBytes = sizeMb * 1048576
+        filedata = filedata.replace('<<APP_NAME>>', f'psize-{sizeMb}')
+        filedata = filedata.replace('<<SIZE_BYTES>>', f'{sizeBytes}')
+
+        # Write the file out again
+        with open(fileName, 'w') as file:
+          file.write(filedata)
+
+    # Create random file and publish
+    os.system("sh create-file.sh")
+    os.system("chalice deploy")
