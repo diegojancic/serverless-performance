af1ad77096fad411a62cc41e3e40aaef49fdee68
Diego Jancic <jancic@gmail.com>
2018-10-22 19:37:00 -0300

    added option to load logs for multiple functions. added stats by function size

--- measure/get-measures.py
+++ measure/get-measures.py
@@ -8,116 +8,109 @@ from scipy import stats
 
 # Read args
 parser = argparse.ArgumentParser(description='Reads CloudWatch Logs and parses execution durations and more.')
-parser.add_argument('function', help='Name of the function to read the info.')
+parser.add_argument('functions', nargs='+', help='Name of the functions to read the info.')
 
 args = parser.parse_args()
+functionNames = args.functions
 
-functionName = args.function
-
-
-functionInfo = {
-	"name": functionName,
-	"memorySize": 0
-}
 data_collected_headers = ["duration", "billed", "memused", "coldStart"]
-data_collected = []
 
-
-print (f"LOADING INFO FOR FUNCTION '{functionName}'")
-print ("-------------------------")
-
-# Load additional function information
-print("Loading function info...")
-lambda_client = boto3.client("lambda")
-finfo = lambda_client.get_function(FunctionName=functionName)
-functionInfo["codeSize"] = finfo["Configuration"]["CodeSize"]/1024/1024
-#functionInfo["codeSize"] = finfo["Configuration"]["MemorySize"]
-functionInfo["inVpc"] = (("VpcConfig" in finfo["Configuration"]) 
-						and finfo["Configuration"]["VpcConfig"]["VpcId"])
-
-
-# Load logs
 cwl = boto3.client('logs')
 
 # Load groups
 print ("Loading log groups...")
 groups = cwl.describe_log_groups()
-
-logGroupName = "/aws/lambda/" + functionName
-logGroups = [g for g in groups["logGroups"] if g["logGroupName"] == logGroupName]
-if len(logGroups) == 0:
-	raise Exception(f"Log group for function '{functionName}' not found.")
-
-logGroup = logGroups[0]
+nextToken = groups["nextToken"] if 'nextToken' in groups else None
+while nextToken:
+	groupsBatch = cwl.describe_log_groups(nextToken=nextToken)
+	nextToken = groupsBatch["nextToken"] if 'nextToken' in groupsBatch else None
+	groups["logGroups"].extend(groupsBatch["logGroups"])
 
 
+def load_function(functionName):
+	functionInfo = {
+		"name": functionName,
+		"memorySize": 0
+	}
+	data_collected = []
 
 
+	print (f"LOADING INFO FOR FUNCTION '{functionName}'")
+	print ("--------------------------------------")
 
-# Read log streams
-logStreams = cwl.describe_log_streams(logGroupName=f'/aws/lambda/{functionName}')
-for logStream in logStreams["logStreams"]:
-	
-	logStreamName = logStream["logStreamName"]
+	# Load additional function information
+	print("Loading function info...")
+	lambda_client = boto3.client("lambda")
+	finfo = lambda_client.get_function(FunctionName=functionName)
+	functionInfo["codeSize"] = finfo["Configuration"]["CodeSize"]/1024/1024
+	#functionInfo["codeSize"] = finfo["Configuration"]["MemorySize"]
+	functionInfo["inVpc"] = (("VpcConfig" in finfo["Configuration"])
+							and finfo["Configuration"]["VpcConfig"]["VpcId"])
 
-	# Read the logs
-	print ("Loading events...")
-	logs = cwl.filter_log_events(logGroupName = f'/aws/lambda/{functionName}', 
-		filterPattern='"Duration: "', 
-		logStreamNames=[logStreamName])
+	# Load logs
 
-	#import pdb;pdb.set_trace()
+	logGroupName = "/aws/lambda/" + functionName
+	logGroups = [g for g in groups["logGroups"] if g["logGroupName"] == logGroupName]
+	if len(logGroups) == 0:
+		raise Exception(f"Log group for function '{functionName}' not found.")
 
-	events = logs["events"]
-	print (f"{len(events)} invocations found...")
-
-	# Parse logs
-	pattern = re.compile(r"Duration: (?P<duration>\d+(\.\d+)?) ms.+Billed Duration: (?P<billed>\d+(\.\d+)?) ms.+Memory Size: (?P<memsize>\d+).+Max Memory Used: (?P<memused>\d+)", 
-				re.IGNORECASE | re.MULTILINE | re.DOTALL)
-
-
-	first_event = True
-	for evnt in events:
-		message = evnt["message"]
-
-		results = pattern.search(message)
-		if not results:
-			print ("Warn: the following message didn't match the pattern: " + message)
-			continue;
-		results.group("duration")
-
-		coldStart = False
-		if first_event:
-			first_event = False
-			coldStart = True
-			functionInfo["memorySize"] = int(results.group("memsize"))
-
-		data_collected.append([
-				float(results.group("duration")),
-				int(results.group("billed")),
-				int(results.group("memused")),
-				coldStart
-			])
+	logGroup = logGroups[0]
 
 
-print ("All Done")
+	# Read log streams
+	logStreams = cwl.describe_log_streams(logGroupName=f'/aws/lambda/{functionName}')
+	for logStream in logStreams["logStreams"]:
 
-# RAW DATA:
-print (functionInfo)
-#print (data_collected_headers)
-#print (data_collected)
+		logStreamName = logStream["logStreamName"]
 
-# SUMMARY DATA:
-data_collected_warm = [x for x in data_collected if x[3] == False]
-data_collected_cold = [x for x in data_collected if x[3] == True]
+		# Read the logs
+		print ("Loading events...")
+		logs = cwl.filter_log_events(logGroupName = f'/aws/lambda/{functionName}',
+			filterPattern='"Duration: "',
+			logStreamNames=[logStreamName])
+
+		#import pdb;pdb.set_trace()
+
+		events = logs["events"]
+		print (f"{len(events)} invocations found...")
+
+		# Parse logs
+		pattern = re.compile(r"Duration: (?P<duration>\d+(\.\d+)?) ms.+Billed Duration: (?P<billed>\d+(\.\d+)?) ms.+Memory Size: (?P<memsize>\d+).+Max Memory Used: (?P<memused>\d+)",
+					re.IGNORECASE | re.MULTILINE | re.DOTALL)
+
+
+		first_event = True
+		for evnt in events:
+			message = evnt["message"]
+
+			results = pattern.search(message)
+			if not results:
+				print ("Warn: the following message didn't match the pattern: " + message)
+				continue;
+			results.group("duration")
+
+			coldStart = False
+			if first_event:
+				first_event = False
+				coldStart = True
+				functionInfo["memorySize"] = int(results.group("memsize"))
+
+			data_collected.append([
+					float(results.group("duration")),
+					int(results.group("billed")),
+					int(results.group("memused")),
+					coldStart
+				])
+
+	return (functionInfo, data_collected)
 
 
 summary_headers = ["Function", "Size(MB)", "VPC", "D.Mean", "Var", "Billed Mean", "Var", "Mem Mean", "Var", "Samples"]
 
-def output_stats(data):
+def get_stats(data):
 	data_stats = stats.describe(data)
 	data_row = [
-				functionName, 
+				functionName,
 				"%.02f" % functionInfo["codeSize"],
 				"Yes" if functionInfo["inVpc"] else "No"
 				]
@@ -137,16 +130,44 @@ def output_stats(data):
 	# Samples
 	data_row.append(data_stats.nobs)
 
-	print(tabulate([data_row], tablefmt="pipe", headers=summary_headers))
+	return data_row
+	#print(tabulate([data_row], tablefmt="pipe", headers=summary_headers))
 
+
+
+### LOAD ALL FUNCTIONS INFO ###
+
+full_results_cold = []
+full_results_warm = []
+for functionName in functionNames:
+	(functionInfo, data_collected) = load_function(functionName)
+
+	data_collected_warm = [x for x in data_collected if x[3] == False]
+	data_collected_cold = [x for x in data_collected if x[3] == True]
+
+	full_results_cold.append(get_stats(data_collected_cold))
+	full_results_warm.append(get_stats(data_collected_warm))
+
+
+print ("All Done")
+
+# RAW DATA:
+print (functionInfo)
+#print (data_collected_headers)
+#print (data_collected)
+
+# SUMMARY DATA:
 
 print("COLD START RESULS")
 print("-----------------")
-output_stats(data_collected_cold)
+print(tabulate(full_results_cold, tablefmt="pipe", headers=summary_headers))
+#output_stats(data_collected_cold)
 
 print("WARM START RESULS")
 print("-----------------")
-output_stats(data_collected_warm)
+print(tabulate(full_results_warm, tablefmt="pipe", headers=summary_headers))
+#output_stats(data_collected_warm)
+
 
 
 #https://pypi.org/project/tabulate/
---
+++ results/by-framework.md
@@ -0,0 +1,49 @@
+# Serverless Performance Tests - Results BY FRAMEWORK
+
+# References
+
+__Columns:__
+
+- Function: the name of the framework(s) used
+- Size: the size of the code as reported by AWS Lambda
+- VPC: Whether the Lambda function is inside a VPC
+- D.Mean: Average (mean) duration of the invocations, as reported by Lambda in CloudWatch Logs.
+- Billed Mean: Average (mean) duration of the billed duration. The billed duration is the duration rounded to the closest 100 milliseconds.
+- Mem: The memory used as reported by Lambda.
+- Var: Variance of the measures of the column on the left.
+
+__Functions:__
+
+- **zappa-django**: Django installation with all Zappa defaults. Set to Debug=False and 512mb Lambda function memory (Zappa's default).
+- **zappa-flask**: Flask installation with all Zappa defaults. Using 512mb Lambda function memory (Zappa's default).
+- **serverless-django**: Django installation using the Serverless framework. Set to Debug=False and 512mb Lambda function memory, with plugins serverless-wsgi and serverless-python-requirements.
+- **serverless-flask**: Flask installation using the Serverless framework. Using 512mb Lambda function memory, with plugins serverless-wsgi and serverless-python-requirements.
+- **chalice**: Basic Chalice example returning a basic HTML.
+- **chalice-1.25mb**: Chalice package plus [5 common Python](functions/chalice-extras/website/requirements.txt) packages. Size 1.25MB.
+
+
+# Results
+
+NOTE: Results are not yet statistically accurate. Not enough samples taken (check back in a few weeks).
+
+### Cold-start statistics
+
+| Function         |   Size(MB) | VPC   |   D.Mean |         Var |   Billed Mean |    Var |   Mem Mean |     Var |   Samples |
+|:-----------------|-----------:|:------|---------:|------------:|--------------:|-------:|-----------:|--------:|----------:|
+| zappa-django |      14.11 | No    |  2271.87 | 1.00807e+06 |       2328.57 | 972381 |    59.4286 | 7.28571 |         7 |
+| zappa-flask |       5.59 | No    |  753.144 | 2552.08 |           820 |  2000 |         47 |     5 |         5 |
+| serverless-django |       9.88 | No    |    82.39 | 10.6617 |           100 |     0 |         49 |     3 |         3 |
+| serverless-flask |       1.42 | No    |  33.9133 | 10.27 |           100 |     0 |         36 |     3 |         3 |
+| chalice |       0.01 | No    |     0.62 | 0.0018 |           100 |     0 |         22 |     0 |         2 |
+| chalice-1.25mb |       1.25 | No    |     0.71 |   nan |           100 |   nan |         22 |   nan |         1 |
+
+### Warm-start statistics
+
+| Function         |   Size(MB) | VPC   |   D.Mean |     Var |   Billed Mean |   Var |   Mem Mean |     Var |   Samples |
+|:-----------------|-----------:|:------|---------:|--------:|--------------:|------:|-----------:|--------:|----------:|
+| zappa-django |      14.11 | No    |  4.19663 | 21.9482 |           100 |     0 |    59.7238 | 6.76771 |       181 |
+| zappa-flask |       5.59 | No    |  4.65865 | 26.103 |           100 |     0 |    47.6474 | 1.64909 |       156 |
+| serverless-django |       9.88 | No    |  4.02335 | 21.8958 |           100 |     0 |     48.422 | 2.25696 |       173 |
+| serverless-flask |       1.42 | No    |  3.02832 | 17.0831 |           100 |     0 |     35.422 | 2.25696 |       173 |
+| chalice |       0.01 | No    |  3.01447 | 14.3451 |           100 |     0 |         22 |     0 |        47 |
+| chalice-1.25mb |       1.25 | No    | 0.645882 | 0.329563 |           100 |     0 |         22 |     0 |        17 |
